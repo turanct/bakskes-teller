01ec4481bb0c948830bc39392f042eb47266a9e1
turanct <spinnewebber_toon@hotmail.com>
2016-04-28 23:15:04 +0200

    Add a SwiftMailer implementation of the RegistrationNotifier
    We want to be able to send real mails

---
+++ src/Authentication/RegistrationNotifierSwiftMailer.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Teller\Authentication;
+
+use Swift_Mailer;
+use Swift_Message;
+
+final class RegistrationNotifierSwiftMailer implements RegistrationNotifier
+{
+    private $mailer;
+
+    public function __construct(Swift_Mailer $mailer)
+    {
+        $this->mailer = $mailer;
+    }
+
+    public function notify(Registration $registration, Secret $secret)
+    {
+        $confirmUrl = 'http://bakske.dev/register/confirm/' . $registration->getEmail();
+        $confirmUrl .= '/' . $secret;
+
+        $body = 'Hallo ' . $registration->getName();
+        $body .= "\n\n" . 'Bevestig je registratie door op deze link te klikken:';
+        $body .= "\n" . $confirmUrl;
+
+        $message = Swift_Message::newInstance('Bakske Verneuzeld Registratie')
+            ->setFrom(array('no-reply@bakske-verneuzeld.be' => 'Bakske Verneuzeld'))
+            ->setTo(array((string) $registration->getEmail() => (string) $registration->getName()))
+            ->setBody($body)
+        ;
+
+        $this->mailer->send($message);
+    }
+}
